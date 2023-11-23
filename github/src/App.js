ff03a19a67731e2affa0175e41a3bcefe4bbb219
Tabrez Alam <97460388+TabrezAlam98@users.noreply.github.com>
2023-11-23 10:31:46 +0530

    productivity

--- github/src/App.js
+++ github/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Productivity from './components/Productivity';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
      <Home/>
+     <Productivity/>
     </div>
   );
 }
---
+++ github/src/components/Productivity.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+// import style from './Productivity.module.css'
+import "./Product.css";
+
+const Productivity = () => {
+  return (
+    <div className="main">
+        <div className="contain">
+        <p className="para1">Productivity</p>
+        <p className="para2">Accelerate innovation</p>
+        <p className="para3">Our AI-powered platform increases the pace of software development.</p>
+        </div>
+      <div className="imgBox">
+        <img src="https://github.githubassets.com/assets/illu-copilot-editor-6474457a5b19.png"/>
+      </div>
+    </div>
+  );
+};
+
+export default Productivity;
